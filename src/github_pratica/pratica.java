1d83b15683b503f9b99132d7a29ca5a8e84c6664
anasilerna <anasmejahad15@alumnes.ilerna.com>
2024-11-13 19:46:13 +0100

    main

--- src/github_pratica/pratica.java
+++ src/github_pratica/pratica.java
@@ -4,6 +4,9 @@ public class pratica {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] sumArray = {4,36,4,23,867};
+		int[] subArray = {785675, 56, 5675, 56544, 234};
+		int[] prodArray = {6, 8, 20, 23, 12};
 
 	}
 
